0878ab288cfa8a8f9be4ec0488adc1ac3b43e651
Nilesh <nileshgulia@gmail.com>
2025-01-14 11:12:28 +0530

    make useFetch generic

--- src/hooks/useFetchInventory.js
+++ src/hooks/useFetchInventory.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-const useFetchInventory = () => {
+const useFetchInventory = (url="https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory", options={}) => {
   const dispatch = useDispatch();
   const inventory = useSelector((state) => state.inventory);
   const [loading, setLoading] = useState(false);
@@ -12,7 +12,7 @@ const useFetchInventory = () => {
     setError(null);
     try {
       const response = await fetch(
-        "https://dev-0tf0hinghgjl39z.api.raw-labs.com/inventory"
+        url,options
       );
       const data = await response.json();
       dispatch({ type: "SET_INVENTORY", payload: data });
